1b8f0a11a2c70d1cbaee310bb0261b77569097b1
Peter <peter.liebau@gmx.de>
2024-05-18 15:29:02 +0200

    new

---
+++ src/env.rs
@@ -0,0 +1,1 @@
+pub const API_KEY:&str = "a2aaaf5c6235a8888808151ae9967999";
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+mod env;
+use reqwest::blocking::get;
+use serde_json::{ Value, from_str };
+
+fn main() {
+    let city: &str = "Halle,DE";
+    let api_key: &str = env::API_KEY;
+    let url = format!("https://api.openweathermap.org/data/2.5/weather?q={}&APPID={}", city, api_key);
+    // println!("URL: {}", url);
+    // println!("\n");
+
+    let response = get(url).unwrap();
+    let response_text = response.text().unwrap();
+    let json: Value = from_str(&response_text).expect("JSON was not well-formatted");
+
+    let temp_kelvin = json["main"]["temp"].as_f64().unwrap();
+    let temp_celsius = temp_kelvin - 273.15;
+    let humidity = json["main"]["humidity"].as_f64().unwrap();
+    let pressure = json["main"]["pressure"].as_f64().unwrap();
+
+    println!("Wetter in {}:", city);
+    println!("  Temperatur : {:.2}°C", temp_celsius);
+    println!("  Luftfeuchte: {}%", humidity);
+    println!("  Luftftdruck: {}hPa", pressure);    
+}
